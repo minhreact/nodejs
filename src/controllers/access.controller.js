f77b89ad66673153ba0029f17d5322a970eee8bc
minhnc1 <minhnc@solashi.com>
2023-05-09 11:23:14 +0700

    feat: handle refreshToken

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -4,6 +4,23 @@ const AccessService = require("../services/access.service");
 const { CREATED, OK, SuccessResponse } = require("../core/success.response");
 
 class AccessController {
+  handlerRefreshToken = async (req, res, next) => {
+    // new SuccessResponse({
+    //   message: "Refresh token success!",
+    //   metadata: await AccessService.handlerRefreshToken(req.body.refreshToken),
+    // }).send(res);
+
+    // v2
+    new SuccessResponse({
+      message: "Refresh token success!",
+      metadata: await AccessService.handlerRefreshTokenV2({
+        keyStore: req.keyStore,
+        user: req.user,
+        refreshToken: req.refreshToken,
+      }),
+    }).send(res);
+  };
+
   logout = async (req, res, next) => {
     new SuccessResponse({
       message: "Logout success!",
